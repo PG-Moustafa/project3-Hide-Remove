4f35fe95ce8f86fd36da7145e3ca5650ec0d9b23
MOUSTAFA HAYDAR <133983067+PG-Moustafa@users.noreply.github.com>
2024-05-21 16:52:07 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+function removeElement1() {
+    document.querySelector('.div1').style.display = "none";
+}
+
+function removeElement2() {
+    document.querySelector('.div4').style.display = "none";
+}
+
+function hideElement1() {
+    document.querySelector('.div2').style.visibility = "hidden";
+}
+
+function hideElement2() {
+    document.querySelector('.div3').style.visibility = "hidden";
+}
+
+function resetAll() {
+    document.querySelector('.div1').style.display = "block";
+    document.querySelector('.div4').style.display = "block";
+    document.querySelector('.div2').style.visibility = "visible";
+    document.querySelector('.div3').style.visibility = "visible";
+}
+
